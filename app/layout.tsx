1af02a8f276805f0492b9772aa03e54179397680
Amlan Sarmah <amlansarmah26@gmail.com>
2024-06-27 19:07:31 +0530

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Innovia | Axon Studio",
+  title: "Innovia",
   description: "Your weekly scoop of tech innovations, trends and insights.",
   
 };
