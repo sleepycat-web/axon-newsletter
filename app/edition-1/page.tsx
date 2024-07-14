d67904a48c93cc902d0682724aea0db79df0dd86
Amlan Sarmah <amlansarmah26@gmail.com>
2024-07-14 15:02:22 +0530

    comm, idk?

--- app/edition-1/page.tsx
+++ app/edition-1/page.tsx
@@ -10,94 +10,189 @@ import Footer from "../../components/ui/footer";
 
 export default function TracingBeamDemo() {
   return (
-    <div className="bg-neutral-950 font-poppins ">
-      <div className="navbar w-full ">
-        <Link href="/" className="btn btn-ghost text-lg">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </Link>
+    <div>
+      {/* Only on Desktop */}
+      <div className="bg-neutral-950 hidden md:block font-poppins ">
+        <div className="navbar w-full ">
+          <Link href="/" className="btn btn-ghost text-lg">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
+        </div>
+        <TracingBeam>
+          <div className="max-w-2xl mx-auto antialiased px-6 pt-4 relative">
+            <div className="mb-10">
+              <p className={twMerge("text-xl mb-4")}>
+                Ever heard of Generative UI?
+              </p>
+              <div className="text-sm prose-sm prose-invert space-y-2.5">
+                <Image
+                  src="/innovia-1/ssat.jpg"
+                  alt="blog thumbnail"
+                  height="1000"
+                  width="1000"
+                  className="rounded-lg  mb-10 object-cover"
+                />
+                <p>
+                  Vercel has recently unveiled their AI sdk kit, which is pretty
+                  impressive.
+                </p>
+
+                <p>
+                  It can dynamically create customized user interfaces in
+                  real-time based on the prompts you provide. Whether you need
+                  components or a full website, it can deliver in HTML or React,
+                  depending on your preference.
+                </p>
+                <p>
+                  And honestly, the results are quite good, with some minor
+                  tweaks they can definitely be used in production.
+                </p>
+              </div>
+            </div>
+
+            <div className="mb-10">
+              <div className="text-sm prose-sm prose-invert">
+                <Image
+                  src="/innovia-1/sshr.jpg"
+                  alt="blog thumbnail"
+                  height="1000"
+                  width="1000"
+                  className="rounded-lg mb-10 object-cover"
+                />
+                <Image
+                  src="/innovia-1/ssfr.jpg"
+                  alt="blog thumbnail"
+                  height="1000"
+                  width="1000"
+                  className="rounded-lg mb-10 object-cover"
+                />
+              </div>
+            </div>
+
+            <div className="mb-10">
+              <p className={twMerge("text-xl mb-4")}>
+                What does this mean for UI/UX designers?
+              </p>
+              <div className="text-sm prose-sm prose-invert space-y-2.5">
+                <p>
+                  Well, this shift will drive a focus on outcome-oriented
+                  design, where designers prioritize user goals and set
+                  constraints for AI to work within, instead of designing
+                  individual interface elements.
+                </p>
+                <p>
+                  Outcome-oriented design emphasises aligning the user
+                  experience with a strong focus on user goals and end results,
+                  while strategically automating elements of interaction and
+                  interface design.
+                </p>
+                <p>
+                  Go to{" "}
+                  <Link
+                    href="https://v0.dev/"
+                    className="text-neutral-400 hover:underline"
+                  >
+                    v0.dev
+                  </Link>{" "}
+                  and check it out.
+                </p>
+              </div>
+            </div>
+          </div>
+        </TracingBeam>
+        <Footer />
       </div>
-      <div className="max-w-2xl mx-auto antialiased px-6 pt-4 relative">
-        <div className="mb-10">
-          <p className={twMerge("text-xl mb-4")}>
-            Ever heard of Generative UI?
-          </p>
-          <div className="text-sm prose-sm prose-invert space-y-2.5">
-            <Image
-              src="/innovia-1/ssat.jpg"
-              alt="blog thumbnail"
-              height="1000"
-              width="1000"
-              className="rounded-lg  mb-10 object-cover"
-            />
-            <p>
-              Vercel has recently unveiled their AI sdk kit, which is pretty
-              impressive.
-            </p>
 
-            <p>
-              It can dynamically create customized user interfaces in real-time
-              based on the prompts you provide. Whether you need components or a
-              full website, it can deliver in HTML or React, depending on your
-              preference.
+      {/* Only on Mobile */}
+      <div className="bg-neutral-950 block md:hidden font-poppins ">
+        <div className="navbar w-full ">
+          <Link href="/" className="btn btn-ghost text-lg">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
+        </div>
+        <div className="max-w-2xl mx-auto antialiased px-6 pt-4 relative">
+          <div className="mb-10">
+            <p className={twMerge("text-xl mb-4")}>
+              Ever heard of Generative UI?
             </p>
-            <p>
-              And honestly, the results are quite good, with some minor tweaks
-              they can definitely be used in production.
+            <div className="text-sm prose-sm prose-invert space-y-2.5">
+              <Image
+                src="/innovia-1/ssat.jpg"
+                alt="blog thumbnail"
+                height="1000"
+                width="1000"
+                className="rounded-lg  mb-10 object-cover"
+              />
+              <p>
+                Vercel has recently unveiled their AI sdk kit, which is pretty
+                impressive.
+              </p>
+
+              <p>
+                It can dynamically create customized user interfaces in
+                real-time based on the prompts you provide. Whether you need
+                components or a full website, it can deliver in HTML or React,
+                depending on your preference.
+              </p>
+              <p>
+                And honestly, the results are quite good, with some minor tweaks
+                they can definitely be used in production.
+              </p>
+            </div>
+          </div>
+
+          <div className="mb-10">
+            <div className="text-sm prose-sm prose-invert">
+              <Image
+                src="/innovia-1/sshr.jpg"
+                alt="blog thumbnail"
+                height="1000"
+                width="1000"
+                className="rounded-lg mb-10 object-cover"
+              />
+              <Image
+                src="/innovia-1/ssfr.jpg"
+                alt="blog thumbnail"
+                height="1000"
+                width="1000"
+                className="rounded-lg mb-10 object-cover"
+              />
+            </div>
+          </div>
+
+          <div className="mb-10">
+            <p className={twMerge("text-xl mb-4")}>
+              What does this mean for UI/UX designers?
             </p>
+            <div className="text-sm prose-sm prose-invert space-y-2.5">
+              <p>
+                Well, this shift will drive a focus on outcome-oriented design,
+                where designers prioritize user goals and set constraints for AI
+                to work within, instead of designing individual interface
+                elements.
+              </p>
+              <p>
+                Outcome-oriented design emphasises aligning the user experience
+                with a strong focus on user goals and end results, while
+                strategically automating elements of interaction and interface
+                design.
+              </p>
+              <p>
+                Go to{" "}
+                <Link
+                  href="https://v0.dev/"
+                  className="text-neutral-400 hover:underline"
+                >
+                  v0.dev
+                </Link>{" "}
+                and check it out.
+              </p>
+            </div>
           </div>
         </div>
 
-        <div className="mb-10">
-          <div className="text-sm prose-sm prose-invert">
-            <Image
-              src="/innovia-1/sshr.jpg"
-              alt="blog thumbnail"
-              height="1000"
-              width="1000"
-              className="rounded-lg mb-10 object-cover"
-            />
-            <Image
-              src="/innovia-1/ssfr.jpg"
-              alt="blog thumbnail"
-              height="1000"
-              width="1000"
-              className="rounded-lg mb-10 object-cover"
-            />
-          </div>
-        </div>
-
-        <div className="mb-10">
-          <p className={twMerge("text-xl mb-4")}>
-            What does this mean for UI/UX designers?
-          </p>
-          <div className="text-sm prose-sm prose-invert space-y-2.5">
-            <p>
-              Well, this shift will drive a focus on outcome-oriented design,
-              where designers prioritize user goals and set constraints for AI
-              to work within, instead of designing individual interface
-              elements.
-            </p>
-            <p>
-              Outcome-oriented design emphasises aligning the user experience
-              with a strong focus on user goals and end results, while
-              strategically automating elements of interaction and interface
-              design.
-            </p>
-            <p>
-              Go to{" "}
-              <Link
-                href="https://v0.dev/"
-                className="text-neutral-400 hover:underline"
-              >
-                v0.dev
-              </Link>{" "}
-              and check it out.
-            </p>
-          </div>
-        </div>
+        <Footer />
       </div>
-      {/* <TracingBeam> </TracingBeam> */}
-      <Footer />
     </div>
   );
 }
--- components/ui/tracing-beam.tsx
+++ components/ui/tracing-beam.tsx
@@ -1,21 +1,33 @@
-
+"use client";
 import React, { useEffect, useRef, useState } from "react";
-import { motion, useTransform, useScroll, useSpring } from "framer-motion";
+import {
+  motion,
+  useTransform,
+  useScroll,
+  useVelocity,
+  useSpring,
+} from "framer-motion";
 import { cn } from "@/lib/utils";
 
-export const TracingBeam = ({ children, className }: { children: React.ReactNode, className: string }) => {
-  const ref = useRef(null);
+export const TracingBeam = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
 
-  const contentRef = useRef(null);
+  const contentRef = useRef<HTMLDivElement>(null);
   const [svgHeight, setSvgHeight] = useState(0);
-  
+
   useEffect(() => {
     if (contentRef.current) {
-      setSvgHeight((contentRef.current as HTMLElement).offsetHeight);
+      setSvgHeight(contentRef.current.offsetHeight);
     }
   }, []);
 
@@ -41,27 +53,47 @@ export const TracingBeam = ({ children, className }: { children: React.ReactNode
     >
       <div className="absolute -left-4 md:-left-20 top-3">
         <motion.div
-          transition={{ duration: 0.2, delay: 0.5 }}
+          transition={{
+            duration: 0.2,
+            delay: 0.5,
+          }}
           animate={{
             boxShadow:
               scrollYProgress.get() > 0
                 ? "none"
                 : "rgba(0, 0, 0, 0.24) 0px 3px 8px",
           }}
-          className="ml-[27px] h-4 w-4 rounded-full shadow-sm flex items-center justify-center"
-        ></motion.div>
+          className="ml-[27px] h-4 w-4 rounded-full  shadow-sm flex items-center justify-center"
+        >
+          {/* <motion.div
+            transition={{
+              duration: 0.2,
+              delay: 0.5,
+            }}
+            animate={{
+              backgroundColor:
+                scrollYProgress.get() > 0 ? "white" : "var(--emerald-500)",
+              borderColor:
+                scrollYProgress.get() > 0 ? "white" : "var(--emerald-600)",
+            }}
+            className="h-2 w-2  rounded-full border border-neutral-300 bg-white"
+          /> */}
+        </motion.div>
         <svg
-          viewBox={`0 0 20 ${svgHeight}`}
+          viewBox={`0 0 20 ${svgHeight }`}
           width="20"
-          height={svgHeight}
-          className="ml-4 block"
+          height={svgHeight} // Set the SVG height
+          className=" ml-4 block"
           aria-hidden="true"
         >
           <motion.path
+            // d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.6} l -18 24V ${svgHeight}`}
             fill="none"
             stroke="#9091A0"
             strokeOpacity="0.16"
-            transition={{ duration: 10 }}
+            transition={{
+              duration: 1,
+            }}
           ></motion.path>
           <motion.path
             d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.6} l -18 24V ${svgHeight}`}
@@ -69,7 +101,9 @@ export const TracingBeam = ({ children, className }: { children: React.ReactNode
             stroke="url(#gradient)"
             strokeWidth="1.25"
             className="motion-reduce:hidden"
-            transition={{ duration: 1 }}
+            transition={{
+              duration: 1,
+            }}
           ></motion.path>
           <defs>
             <motion.linearGradient
@@ -77,8 +111,8 @@ export const TracingBeam = ({ children, className }: { children: React.ReactNode
               gradientUnits="userSpaceOnUse"
               x1="0"
               x2="0"
-              y1={y1}
-              y2={y2}
+              y1={y1} // set y1 for gradient
+              y2={y2} // set y2 for gradient
             >
               <stop stopColor="#18CCFC" stopOpacity="0"></stop>
               <stop stopColor="#18CCFC"></stop>
