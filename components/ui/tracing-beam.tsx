45e55be5712ba88605d43ac11e4fccf5ea7b0481
Amlan Sarmah <amlansarmah26@gmail.com>
2024-07-14 18:05:36 +0530

    om hari nath

--- components/ui/tracing-beam.tsx
+++ components/ui/tracing-beam.tsx
@@ -79,7 +79,7 @@ export const TracingBeam = ({
         <svg
           viewBox={`0 0 20 ${svgHeight}`}
           width="20"
-          height={svgHeight} // Set the SVG height
+          height="100%"
           className="ml-4 block"
           aria-hidden="true"
         >
