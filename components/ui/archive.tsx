91629c2f42a8701e4db353df58835af288e26601
Amlan Sarmah <amlansarmah26@gmail.com>
2024-07-14 13:21:17 +0530

    home fix

--- components/ui/archive.tsx
+++ components/ui/archive.tsx
@@ -32,10 +32,15 @@ const Archive: React.FC = () => {
 
   return (
     <section className="bg-neutral-950 font-poppins text-neutral-400 body-font ">
-      
-      
-
       <div className="container px-5 py-24 mx-auto">
+
+        <div className="lg:w-1/2 w-full mb-6 ">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+            Archive
+          </h1>
+          <div className="h-1 w-20 bg-neutral-500 rounded"></div>
+        </div>
+
         <div className="flex flex-wrap -m-4">
           {items.map((item) => (
             <div key={item.id} className="p-4 md:w-1/3">
