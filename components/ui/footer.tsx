05881948eda3f2a261cb194448951f2d23fb2978
Amlan Sarmah <amlansarmah26@gmail.com>
2024-07-14 13:03:32 +0530

    adder

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -5,17 +5,18 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className=" footer grid grid-flow-col bg-neutral-950 p-6 py-8 gap-4 text-neutral-content">
       <aside>
         <a href="https://axonstudio.tech/" rel="noopener noreferrer">
-          <img
+          <Image
             className="h-12 w-auto"
-            width="65"
-            height="40"
             src="/logo.png"
             alt="Logo"
+            width={65}
+            height={40}
           />
         </a>
         <Link
